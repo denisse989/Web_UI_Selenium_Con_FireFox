66d13b0dde16a2eb7e9dff0f44fcd792419e36cc
denisse989 <denissebaldiviesopacheco@gmail.com>
2022-03-03 18:47:39 -0400

    2P Selenium usando FireFox

--- src/test/java/tareaSeleniumuConFireFox/BasicSelenium.java
+++ src/test/java/tareaSeleniumuConFireFox/BasicSelenium.java
@@ -1,4 +1,26 @@
 package tareaSeleniumuConFireFox;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BasicSelenium {
+    WebDriver firefoxDriver ;
+    @BeforeEach
+    public void openBrowser(){
+        System.setProperty("webdriver.gecko.driver","src/test/resources/driver/geckodriver.exe");
+        firefoxDriver = new FirefoxDriver();
+        firefoxDriver.get("https://www.google.com/");
+    }
+    @Test
+    public void goToFireFox() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+
+    @AfterEach
+    public void closeBrowser(){
+        firefoxDriver.quit();
+    }
 }
